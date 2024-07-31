bcecfc0867707706da2c714f3f6071dd1e35214c
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2024-07-31 15:40:38 +0530

    Updated

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 from g4f.client import Client
 import asyncio
-from typing import Union
 import sys
 import platform
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 if platform.system() == "Windows":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 app = FastAPI()
 
-@app.get("/")
-async def show_error():
-    return {"error" : "Authentication required."}
-
-@app.post("/promt/{user_promt}")
-async def read_promt(user_promt: str):
+@app.post("/")
+async def read_prompt(prompt: str = Form(...)):
     client = Client()
     try:
         response = client.chat.completions.create(
             model="gpt-4o",
-            messages=[{"role": "user", "content": f"{user_promt}"}],
+            messages=[{"role": "user", "content": f"{prompt}"}],
         )
         reply = response.choices[0].message.content
-        return {"response" : reply}
-    except:
-        return {"error" : "Something wrong!"}
-
+        return {"response": reply}
+    except Exception as e:
+        return {"error": f"Something went wrong: {str(e)}"}
