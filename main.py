a27a62fe1df0c89cd531489bf7d4204be32773ac
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2024-07-31 14:20:15 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from g4f.client import Client
+import asyncio
+from typing import Union
+
+from fastapi import FastAPI
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+app = FastAPI()
+
+@app.get("/")
+def show_error():
+    return {"error" : "Authentication required."}
+
+@app.post("/promt/{user_promt}")
+def read_promt(user_promt: str):
+    client = Client()
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "user", "content": f"{user_promt}"}],
+        )
+        reply = response.choices[0].message.content
+        return {"response" : reply}
+    except:
+        return {"error" : "Something wrong!"}
+
