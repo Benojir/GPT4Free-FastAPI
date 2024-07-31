3cde5dfa4b8eb48ddf17b2a86ccbb3c4b1612f68
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2024-07-31 16:59:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from g4f.client import Client
-import asyncio
 import sys
 import platform
 
 from fastapi import FastAPI, Form
 
-if platform.system() == "Windows":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
 app = FastAPI()
 
 @app.post("/")
-async def read_prompt(prompt: str = Form(...)):
+def read_prompt(prompt: str = Form(...)):
     client = Client()
     try:
         response = client.chat.completions.create(
